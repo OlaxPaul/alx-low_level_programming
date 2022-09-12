459596101061c524728ccb087fc6c6c6000698e8
OlaxPaul <paulsaxx2016@gmail.com>
2022-09-11 20:35:56 -0700

    2. I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - To write a program that prints alphabets in lowercase.
+ *
+ * Description: "Print alphabets in lowercase."
+ *
+ * Return: Always 0 (success)
+ *
+ */
+
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	        {  
+		putchar(ch);
+	        }
+
+	putchar('\n');
+	return (0);
+}
