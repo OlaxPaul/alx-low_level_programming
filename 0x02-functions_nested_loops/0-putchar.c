595a96702976bf01bba001acac74ca78cb1ec5ac
OlaxPaul <paulsaxx2016@gmail.com>
2022-09-13 04:49:31 -0700

    0. _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - Program entry point
+ *
+ * Description: This is a program that will print the message "_putchar" to the screen 
+ * followed by a new line. It uses a locally defined function (_putchar) to accomplish this.
+ *
+ * Return: Always 0 (If successful)
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0)
+}
