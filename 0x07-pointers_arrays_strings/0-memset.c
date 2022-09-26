953d21204601b5e8473938bafc2231c3891e27e2
OlaxPaul <paulsaxx2016@gmail.com>
2022-09-26 10:32:14 -0700

    0. memset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -5,16 +5,15 @@
  * @s: address to memory block
  * @b: char to be used
  * @N: number of bytes to be used
- *
  * Return: pointer to the memory block
  */
 
 char *_memset(char *s, char b, unsigned int n)
 {
-	while(n)
+	while (n)
 	{
 		s[n - 1] = b;
 		n--;
 	}
-	return(s);
+	return (s);
 }
