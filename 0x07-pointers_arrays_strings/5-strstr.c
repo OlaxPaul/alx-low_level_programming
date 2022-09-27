bc84781d689332f4a5d9a099b004fa3409785fba
OlaxPaul <paulsaxx2016@gmail.com>
2022-09-27 10:37:59 -0700

    5. strstr

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -4,8 +4,7 @@
  * _strstr - locates a substring
  * @haystack: string in which to check for needle
  * @needle: substring to find in haystack
- *
- * Return: pointer to beginning of needle in haystack or NULL if no match
+ * Return: 0
  */
 char *_strstr(char *haystack, char *needle)
 {
